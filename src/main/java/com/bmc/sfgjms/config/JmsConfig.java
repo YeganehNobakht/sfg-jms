07630569834a7862e2e321ec0c53d5ef146fbb2e
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-23 13:31:19 +0330

    adding message converter bean

---
+++ src/main/java/com/bmc/sfgjms/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.bmc.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author Masoumeh Yeganeh
+ * @created 23/05/2023
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
