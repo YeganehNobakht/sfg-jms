f8d10da602a3b7840d1a443986e98dcd3eac015a
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-23 13:24:07 +0330

    adding task executer config

---
+++ src/main/java/com/bmc/sfgjms/config/TaskConfig.java
@@ -0,0 +1,24 @@
+package com.bmc.sfgjms.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Masoumeh Yeganeh
+ * @created 23/05/2023
+ */
+@Configuration
+@EnableCaching
+@EnableAsync
+//this combination of annotations sets up spring boot to go ahead and perform tasks out of a task pool
+public class TaskConfig {
+
+    @Bean
+    TaskExecutor taskExecutor(){
+        return new SimpleAsyncTaskExecutor();
+    }
+}
