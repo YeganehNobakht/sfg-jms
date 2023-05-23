7d4f86af6dbe27d163c730ba37715a38e1dc49b7
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-23 15:29:48 +0330

    send and recieve of jms messages

--- src/main/java/com/bmc/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/com/bmc/sfgjms/listener/HelloMessageListener.java
@@ -2,12 +2,17 @@ package com.bmc.sfgjms.listener;
 
 import com.bmc.sfgjms.config.JmsConfig;
 import com.bmc.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.jgroups.util.UUID;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 /**
@@ -15,7 +20,10 @@ import javax.jms.Message;
  * @created 23/05/2023
  */
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
@@ -24,6 +32,20 @@ public class HelloMessageListener {
         System.out.println("I got a message");
 
         System.out.println(helloWorldMessage);
+
+    }
+
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payload = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("world!!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payload);
 
     }
 }
--- src/main/java/com/bmc/sfgjms/sender/HelloSender.java
+++ src/main/java/com/bmc/sfgjms/sender/HelloSender.java
@@ -2,11 +2,18 @@ package com.bmc.sfgjms.sender;
 
 import com.bmc.sfgjms.config.JmsConfig;
 import com.bmc.sfgjms.model.HelloWorldMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.jgroups.util.UUID;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 
 /**
  * @author Masoumeh Yeganeh
@@ -17,6 +24,7 @@ import org.springframework.stereotype.Component;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
@@ -31,6 +39,41 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
         System.out.println("Message sent!");
+
+    }
+
+
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello world")
+                .build();
+
+        Message receiveMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RECEIVE_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+
+                Message helloMessage = null;
+
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "com.bmc.sfgjms.model.HelloWorldMessage");
+
+                    System.out.println("sending hello");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+            }
+        });
+
+        System.out.println(receiveMessage.getBody(String.class));
 
     }
 }
