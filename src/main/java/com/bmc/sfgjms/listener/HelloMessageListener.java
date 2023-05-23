ff5b3d696ddd00bacd8335400546123dcf0d2764
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-23 14:44:50 +0330

    adding jms message listener

---
+++ src/main/java/com/bmc/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,29 @@
+package com.bmc.sfgjms.listener;
+
+import com.bmc.sfgjms.config.JmsConfig;
+import com.bmc.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+/**
+ * @author Masoumeh Yeganeh
+ * @created 23/05/2023
+ */
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+        System.out.println("I got a message");
+
+        System.out.println(helloWorldMessage);
+
+    }
+}
